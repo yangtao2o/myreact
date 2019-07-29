c47deb3ad1a952ae2ff3bf780bb701ac321b5a52
yangtao <istaotao@aliyun.com>
2019-07-29 14:09:16 +0800

    Update styling commonents about next.js

--- nextjs/01-hello-next/pages/index.js
+++ nextjs/01-hello-next/pages/index.js
@@ -14,6 +14,29 @@ const Index = (props) => (
         </li>
       ))}
     </ul>
+    <style jsx>{`
+      h1, a {
+        font-family: "Arial";
+      }
+
+      ul {
+        padding: 0;
+      }
+
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
   </Layout>
 );
 
