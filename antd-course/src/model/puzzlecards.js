cde5ad9ed3db0d070ac715fe2d83b29a510cd01a
yangtao <istaotao@aliyun.com>
2019-12-14 12:27:23 +0800

    feat: 使用 connect 对接静态的 dva model

---
+++ antd-course/src/model/puzzlecards.js
@@ -0,0 +1,15 @@
+export default {
+  namespace: "puzzlecards",
+  state: [
+    {
+      id: 1,
+      setup: "Did you hear about the two silk worms in a race?",
+      punchline: "It ended in a tie"
+    },
+    {
+      id: 2,
+      setup: "What happens to a frog's car when it breaks down?",
+      punchline: "It gets toad away"
+    }
+  ]
+};
--- antd-course/src/page/puzzlecards.js
+++ antd-course/src/page/puzzlecards.js
@@ -1,46 +1,23 @@
 import React, { Component } from "react";
-import { Card, Button } from "antd";
+import { Card /* ,Button */ } from "antd";
+import { connect } from "dva";
+
+const namespace = "puzzlecards";
+
+const mapStateToProps = state => {
+  const cardList = state[namespace];
+  return {
+    cardList
+  };
+};
+
+@connect(mapStateToProps)
 
 export default class PuzzleCardsPage extends Component {
-  constructor(props) {
-    super(props);
-    this.counter = 100;
-    this.state = {
-      cardList: [
-        {
-          id: 1,
-          setup: "Did you hear about the two silk worms in a race?",
-          punchline: "It ended in a tie"
-        },
-        {
-          id: 2,
-          setup: "What happens to a frog's car when it breaks down?",
-          punchline: "It gets toad away"
-        }
-      ]
-    };
-  }
-
-  addNewCard = () => {
-    this.setState(prevState => {
-      const prevCardList = prevState.cardList;
-      this.counter += 1;
-      const card = {
-        id: this.counter,
-        setup: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
-        punchline:
-          "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-      };
-      return {
-        cardList: prevCardList.concat(card)
-      };
-    });
-  };
-
   render() {
     return (
       <div>
-        {this.state.cardList.map(card => {
+        {this.props.cardList.map(card => {
           return (
             <Card key={card.id}>
               <div>Q: {card.setup}</div>
@@ -50,9 +27,9 @@ export default class PuzzleCardsPage extends Component {
             </Card>
           );
         })}
-        <div>
+        {/* <div>
           <Button onClick={this.addNewCard}> 添加卡片 </Button>
-        </div>
+        </div> */}
       </div>
     );
   }
