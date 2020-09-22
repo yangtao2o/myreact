42fa79b0323a30496820d804cda18535d01ae8f0
杨涛 <tao.yang@kxhz.cc>
2020-09-22 10:46:44 +0800

    feat: add 03-demo-ssr

--- nextjs/01-hello-next/pages/post.js
+++ nextjs/01-hello-next/pages/post.js
@@ -1,22 +1,33 @@
-import Layout from '../components/MyLayout.js';
-import fetch from 'isomorphic-unfetch';
+import Layout from '../components/MyLayout.js'
+import fetch from 'isomorphic-unfetch'
 
-const Post = (props) => (
-  <Layout>
-    <h1>{props.show.name}</h1>
-    <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
-    <img src={props.show.image.medium} />
-  </Layout>
-);
+const Post = props => {
+  if (props && props.show) {
+    return (
+      <Layout>
+        <h1>{props.show.name}</h1>
+        <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
+        <img src={props.show.image ? props.show.image.medium : ''} />
+      </Layout>
+    )
+  } else {
+    return (
+      <Layout>
+        <h1>Not Found</h1>
+      </Layout>
+    )
+  }
+}
 
 Post.getInitialProps = async function (context) {
-  const {id} = context.query;
-  const res = await fetch(`http://api.tvmaze.com/shows/${id}`);
-  const show = await res.json();
+  console.log({ context })
+  const { id } = context.query
+  const res = await fetch(`http://api.tvmaze.com/shows/${id}`)
+  const show = await res.json()
 
-  console.log(`Fetched show: ${show.name}`);
+  console.log(`Fetched show: ${show.name}`)
 
-  return {show}
-};
+  return { show }
+}
 
-export default Post;
+export default Post
