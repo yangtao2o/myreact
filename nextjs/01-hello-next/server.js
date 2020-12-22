79953760bf8cc37e49d6b8782d8543a177825beb
杨涛 <tao.yang@kxhz.cc>
2020-12-22 16:33:15 +0800

    chore: next添加获取本地ip配置

--- nextjs/01-hello-next/server.js
+++ nextjs/01-hello-next/server.js
@@ -1,30 +1,52 @@
-const express = require('express');
-const next = require('./node_modules/next');
+const express = require('express')
+const os = require('os')
+const next = require('./node_modules/next')
 
-const dev = process.env.NODE_ENV != 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const dev = process.env.NODE_ENV != 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+const PORT = 3101
+const myHost = getLocalIP()
 
-app.prepare().then(() => {
-  const server = express();
+function getLocalIP() {
+  const interfaces = os.networkInterfaces()
+  for (let devName in interfaces) {
+    const iface = interfaces[devName]
+    for (var i = 0; i < iface.length; i++) {
+      const alias = iface[i]
+      if (
+        alias.family === 'IPv4' &&
+        alias.address !== '127.0.0.1' &&
+        !alias.internal
+      ) {
+        return alias.address
+      }
+    }
+  }
+}
 
-  server.get('/p/:id', (req, res) => {
-    const actualPage = '/post';
-    const queryParams = { id: req.params.id};
-    console.log('queryParams: ', queryParams);
-    app.render(req, res, actualPage, queryParams);
+app
+  .prepare()
+  .then(() => {
+    const server = express()
+
+    server.get('/p/:id', (req, res) => {
+      const actualPage = '/post'
+      const queryParams = { id: req.params.id }
+      console.log('queryParams: ', queryParams)
+      app.render(req, res, actualPage, queryParams)
+    })
+
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    })
+
+    server.listen(PORT, err => {
+      if (err) throw err
+      console.log(`Server running at http://${myHost}:${PORT}`)
+    })
   })
-  
-  server.get('*', (req, res) => {
-    return handle(req, res);
-  });
-
-  server.listen(3000, (err) => {
-    if(err) throw err;
-    console.log('> Ready on http://localhost:3000');
-  });
-}).catch((ex) => {
-  console.error(ex.stack);
-  process.exit(1);
-
-});
+  .catch(ex => {
+    console.error(ex.stack)
+    process.exit(1)
+  })
