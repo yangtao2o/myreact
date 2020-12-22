dd5dcdf9afce315418f9004b08de839a9ab87813
杨涛 <tao.yang@kxhz.cc>
2020-12-22 14:06:17 +0800

    feat: 动手实现 Redux（四）：共享结构的对象提高性能

--- myredux/make-redux/src/index.js
+++ myredux/make-redux/src/index.js
@@ -12,13 +12,25 @@ const appState = {
 function stateChanger(state, action) {
   switch (action.type) {
     case 'UPDATE_TITLE_TEXT':
-      state.title.text = action.text
-      break
+      return {
+        // 构建新的对象并且返回
+        ...state,
+        title: {
+          ...state.title,
+          text: action.text,
+        },
+      }
     case 'UPDATE_TITLE_COLOR':
-      state.title.color = action.color
-      break
+      return {
+        // 构建新的对象并且返回
+        ...state,
+        title: {
+          ...state.title,
+          color: action.color,
+        },
+      }
     default:
-      break
+      return state // 没有修改，返回原来的对象
   }
 }
 
@@ -27,7 +39,7 @@ function createStore(state, stateChanger) {
   const subscribe = listener => listeners.push(listener)
   const getState = () => state
   const dispatch = action => {
-    stateChanger(state, action)
+    state = stateChanger(state, action) // 覆盖原对象
     listeners.forEach(listener => listener())
   }
   return {
@@ -37,27 +49,41 @@ function createStore(state, stateChanger) {
   }
 }
 
-function renderApp(appState) {
-  renderTitle(appState.title)
-  renderContent(appState.content)
+function renderApp(newAppState, oldAppState = {}) {
+  // 防止 oldAppState 没有传入，所以加了默认参数 oldAppState = {}
+  if (newAppState === oldAppState) return // 数据没有变化就不渲染了
+  console.log('render app...')
+  renderTitle(newAppState.title, oldAppState.title)
+  renderContent(newAppState.content, oldAppState.content)
 }
 
-function renderTitle(title) {
+function renderTitle(newTitle, oldTitle = {}) {
+  if (newTitle === oldTitle) return // 数据没有变化就不渲染了
+  console.log('render title...')
   const titleDOM = document.getElementById('title')
-  titleDOM.innerHTML = title.text
-  titleDOM.style.color = title.color
+  titleDOM.innerHTML = newTitle.text
+  titleDOM.style.color = newTitle.color
 }
 
-function renderContent(content) {
+function renderContent(newContent, oldContent = {}) {
+  if (newContent === oldContent) return // 数据没有变化就不渲染了
+  console.log('render content...')
   const contentDOM = document.getElementById('content')
-  contentDOM.innerHTML = content.text
-  contentDOM.style.color = content.color
+  contentDOM.innerHTML = newContent.text
+  contentDOM.style.color = newContent.color
 }
 
 const store = createStore(appState, stateChanger)
-// 监听数据变化
-store.subscribe(() => renderApp(store.getState()))
+let oldState = store.getState() // 缓存旧的 state
 
+// 监听数据变化
+store.subscribe(() => {
+  const newState = store.getState() // 数据可能变化，获取新的 state
+  renderApp(newState, oldState) // 把新旧的 state 传进去渲染
+  oldState = newState // 渲染完以后，新的 newState 变成了旧的 oldState，等待下一次数据变化重新渲染
+})
 renderApp(store.getState()) // 首次渲染页面
+console.log('=============')
 store.dispatch({ type: 'UPDATE_TITLE_TEXT', text: '《React.js 小书》' }) // 修改标题文本
+console.log('=============')
 store.dispatch({ type: 'UPDATE_TITLE_COLOR', color: 'blue' }) // 修改标题颜色
