09d3943eab1707701a003c5b070d0e2722562747
杨涛 <tao.yang@kxhz.cc>
2020-12-23 13:41:30 +0800

    feat: 动手实现 React-redux（一）：初始化工程

---
+++ myredux/make-react-redux/src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/Header'
+import Content from './components/Content'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Content />
+    </div>
+  )
+}
+
+export default App
